b6775d3d16ebed51a275dc0266d05dc4d9a6e9d7
Kamla Kanwar <159700163+kamlakanwar@users.noreply.github.com>
2025-07-28 23:46:51 +0530

    Update Cards.jsx

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Card from './Card';
+import Card from './Card'; // make sure path is correct
 
-const Cards = ({ productData }) => {
+const Cards = ({productData}) => {
+
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4">
+    <div className="flex flex-wrap justify-center gap-6 p-4">
       {productData.map((item) => (
-        <Card
-          key={item.id}
+        <Card key={item.id}
           id={item.id}
           img={item.img}
           title={item.title}
