04eb506799d2d7ee72fc0b98a4ecda7a95828e0b
Kamla Kanwar <159700163+kamlakanwar@users.noreply.github.com>
2025-08-01 17:32:08 +0530

    Update Cards.jsx

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -4,7 +4,7 @@ import Card from './Card'; // make sure path is correct
 const Cards = ({productData}) => {
 
   return (
-    <div className="flex flex-wrap justify-center gap-4 m-20">
+    <div className="flex flex-wrap justify-center gap-4 mx-10">
       {productData.map((item) => (
         <Card key={item.id}
           id={item.id}
